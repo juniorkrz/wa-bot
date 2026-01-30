744faca6c5d7a671aec21d7ec84b640df22f8925
Krz <juniorkrz77@gmail.com>
2026-01-30 18:14:03 -0300

    baileys helpers

---
+++ src/helpers/baileys.ts
@@ -0,0 +1,29 @@
+import { GroupMetadata, jidNormalizedUser, proto } from '@whiskeysockets/baileys'
+import { getSocket } from '../bot.js'
+
+export function getQuotedMessage(
+  message: proto.IWebMessageInfo
+) {
+  return message.message?.extendedTextMessage?.contextInfo?.quotedMessage
+}
+
+export const getPhoneFromJid = (jid: string | undefined) => {
+  return jidNormalizedUser(jid).replace(/\D/g, '')
+}
+
+const normalizeLid = (lid: string) =>
+  lid.split(':')[0] + '@lid'
+
+export const amAdminOfGroup = (group?: GroupMetadata): boolean => {
+  if (!group) return false
+
+  const sock = getSocket()
+  const myLid = sock.user?.lid
+  if (!myLid) return false
+
+  const me = group.participants.find(
+    (p) => p.id === normalizeLid(myLid)
+  )
+
+  return me?.admin === 'admin' || me?.admin === 'superadmin'
+}
