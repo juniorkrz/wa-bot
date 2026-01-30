89d3beadc0b688dddde73173d652332fead45ee9
Krz <juniorkrz77@gmail.com>
2026-01-30 18:14:20 -0300

    ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,27 @@
-import { Command } from "../types/command.js"
+import { botConfig } from "../config/bot.js"
+import { emojis } from "../helpers/emojis.js"
+import { getRandomItemFromArray, spintax } from "../helpers/utils.js"
+import { Command } from "../types/Command.js"
 
 const ping: Command = {
     name: 'ping',
     aliases: ['teste'],
     description: 'Responde pong',
 
-    async execute({ sock, jid }) {
-        await sock.sendMessage(jid, { text: 'pong 🏓' })
+    async execute(ctx) {
+        const { timestamp } = ctx
+        // Responde 'Pong' e reage a mensagem:
+        const time = <number>timestamp
+        const ms = Date.now() - time
+
+        // Mensagem a ser enviada
+        const text = `Pong! ${emojis.ping}\n\n${getRandomItemFromArray(emojis.wait)} ` +
+            `Tempo de resposta do {${botConfig.name}|bot} foi de *${ms}ms*.`
+
+        const reply = spintax(text)
+
+        await ctx.reply(reply)
+        await ctx.react(getRandomItemFromArray(emojis.ping))
     }
 }
 
